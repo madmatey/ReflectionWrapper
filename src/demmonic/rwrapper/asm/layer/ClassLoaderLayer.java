91413922c9b735745b1d50d2561023e6828847c5
demmonic <apocalypse9xx@hotmail.com>
2014-03-19 03:56:35 -0400

    added class loader layer

---
+++ src/demmonic/rwrapper/asm/layer/ClassLoaderLayer.java
@@ -0,0 +1,50 @@
+package demmonic.rwrapper.asm.layer;
+
+import java.security.ProtectionDomain;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+
+import demmonic.rwrapper.Loader;
+
+/**
+ * 
+ * @author Demmonic
+ *
+ * We can't let any newly defined classes escape our iron grip >:)
+ */
+public class ClassLoaderLayer extends ClassLoader {
+
+	/**
+	 * @param b
+	 * 			The class to secure
+	 * @return Secured class
+	 */
+	private byte[] secure(byte[] b) {
+		ClassReader cr = new ClassReader(b);
+		ClassNode cn = new ClassNode();
+		cr.accept(cn, 0);
+		
+		Loader.secure(cn);
+		
+		ClassWriter cw = new ClassWriter(0);
+		cn.accept(cw);
+		
+		return cw.toByteArray();
+	}
+	
+	//DUPLICATE METHODS FOR MIMICING THE SYSTEM CLASS IN THE RT LIBRARY
+	protected Class<?> ourDefineClass(String name, byte[] b, int off, int len) {
+		b = secure(b);
+		
+		return super.defineClass(name, b, off, b.length);
+	}
+	
+	protected Class<?> ourDefineClass(String name, byte[] b, int off, int len, ProtectionDomain protectionDomain) {
+		b = secure(b);
+		
+		return super.defineClass(name, b, off, b.length, protectionDomain);
+	}
+	
+}
