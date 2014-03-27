5427bfdccd70b1549d472eaf61d3d68f6498c3eb
demmonic <apocalypse9xx@hotmail.com>
2014-03-27 06:57:04 -0400

    fixed reference swapping

--- src/demmonic/rwrapper/util/ASMUtil.java
+++ src/demmonic/rwrapper/util/ASMUtil.java
@@ -4,6 +4,7 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.LocalVariableNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.TypeInsnNode;
@@ -49,6 +50,16 @@ public final class ASMUtil {
 						fin.owner = to;
 					}
 					fin.desc = fin.desc.replace("L" + from + ";", "L" + to + ";");
+				}
+			}
+			
+			mn.desc = mn.desc.replace("L" + from + ";", "L" + to + ";");
+			
+			if (mn.localVariables != null) {
+				for (LocalVariableNode n : mn.localVariables) {
+					if (n.desc.equals("L" + from + ";")) {
+						n.desc = "L" + to + ";";
+					}
 				}
 			}
 		}
