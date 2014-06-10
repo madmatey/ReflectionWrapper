e59d156511e42aa4d935595c3df70f595c7af3fb
madmatey <nick_pro@live.nl>
2014-06-10 16:13:10 +0200

    Update GenericClientLoader.java

--- example/demmonic/rwrapper/loader/generic/GenericClientLoader.java
+++ example/demmonic/rwrapper/loader/generic/GenericClientLoader.java
@@ -22,7 +22,7 @@ public class GenericClientLoader extends ClientLoader {
 	@Override
 	public InputStream getClient() {
 		try {
-			return new FileInputStream("C:\Users\Martijn\Desktop\salusscape.jar");
+			return new FileInputStream("C:\Users\Martijn\Desktop\salus.jar");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
